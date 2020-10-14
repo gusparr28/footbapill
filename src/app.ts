e9eed1f9eee9bedf43a1d420d03a21122ce8484b
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-14 07:57:51 -0400

    File upload done

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
 import passportMiddleware from './middlewares/passport';
+import path from 'path';
 const app = express();
 
 import authRoutes from './routes/auth';
+import fileRoute from './routes/file';
 
 // middlewares
 app.use(cors());
@@ -16,6 +18,7 @@ passport.use(passportMiddleware);
 
 // routes
 app.use(authRoutes);
+app.use(fileRoute, express.static(path.resolve('uploads')));
 
 // settings
 app.set('port', process.env.PORT || 3000);
