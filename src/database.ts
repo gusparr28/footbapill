6167c13bf9bb7a1d5c82289b09fdccf75410a7a2
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-15 16:32:36 -0400

    Fixing Heroku issues

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
-// require('dotenv').config();
+require('dotenv').config();
 
 export const pool = new Pool({
-    connectionString: 'postgres://aorbivcutbdxue:bd016d417aaf9c96da458a29573e2f436d25513e4c7185558b60fa531c537b42@ec2-52-3-4-232.compute-1.amazonaws.com:5432/dfh1v31ete2n74',
-    ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.connect().then(() => console.log("Successfully connected to PostgreSQL"))
