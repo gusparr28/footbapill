f1cb1bbae3774524876214c8181dd0a8bf7b32f5
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-15 14:30:30 -0400

    Setting Procfile

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg';
+require('dotenv').config();
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'apifutbolweb',
+    user: 'aorbivcutbdxue',
+    host: 'ec2-52-3-4-232.compute-1.amazonaws.com',
+    password: 'bd016d417aaf9c96da458a29573e2f436d25513e4c7185558b60fa531c537b42',
+    database: 'dfh1v31ete2n74',
     port: 5432
 });
 
