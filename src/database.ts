6c12a09d7f12af576daf4e6a985ea4e68d1cd5c6
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-15 16:21:16 -0400

    Deploying to Heroku

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,9 @@
 import { Pool } from 'pg';
-require('dotenv').config();
+// require('dotenv').config();
 
 export const pool = new Pool({
-    user: 'aorbivcutbdxue',
-    host: 'ec2-52-3-4-232.compute-1.amazonaws.com',
-    password: 'bd016d417aaf9c96da458a29573e2f436d25513e4c7185558b60fa531c537b42',
-    database: 'dfh1v31ete2n74',
-    port: 5432
+    connectionString: 'postgres://aorbivcutbdxue:bd016d417aaf9c96da458a29573e2f436d25513e4c7185558b60fa531c537b42@ec2-52-3-4-232.compute-1.amazonaws.com:5432/dfh1v31ete2n74',
+    ssl: true
 });
 
 pool.connect().then(() => console.log("Successfully connected to PostgreSQL"))
